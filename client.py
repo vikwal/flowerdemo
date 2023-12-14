015948e427276798f6792d0d303ed373b76ac590
Viktor Walter <vi.wa.kgt@gmail.com>
2023-12-14 14:36:20 +0100

    added methods to client and server

--- client.py
+++ client.py
@@ -1,33 +1,55 @@
+import argparse
 import os
+import json
 
 import flwr as fl
 import tensorflow as tf
+from tensorflow import keras
 
 
-# Make TensorFlow log less verbose
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+# Hyperparameters
+server_address = '127.0.0.1:8080'
+verbose = 0
+client_nr = 0
 
-# Load model and data (MobileNetV2, CIFAR-10)
-model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
-model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
+# partitioning data
+start_index = client_nr * 12500 
+end_index = 12500 * (client_nr + 1)
+x_train, y_train = x_train[start_index:end_index], y_train[start_index:end_index]
+
+start_index = client_nr * 2500 
+end_index = 2500 * (client_nr + 1)
+x_test, y_test = x_test[start_index:end_index], y_test[start_index:end_index]
 
 # Define Flower client
-class CifarClient(fl.client.NumPyClient):
+class Client(fl.client.NumPyClient):
+    def __init__(self, model):
+        self.model = model
+
     def get_parameters(self, config):
-        return model.get_weights()
+        return self.model.get_weights()
 
     def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32)
-        return model.get_weights(), len(x_train), {}
+        model_config = json.loads(config['model']) # use json.loads() to convert it back to dict type
+        self.model = keras.Model().from_config(model_config)
+        self.model.compile(optimizer=config['optimizer'], loss=config['loss'], metrics=config['metrics'])
+
+        self.model.set_weights(parameters)
+
+        batch_size: int = config['batch_size']
+        epochs: int = config['local_epochs']
+
+        self.model.set_weights(parameters)
+        self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+        return self.model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test)
+        self.model.set_weights(parameters)
+        loss, accuracy = self.model.evaluate(x_test, y_test)
         return loss, len(x_test), {"accuracy": accuracy}
 
 
 # Start Flower client
-fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=CifarClient())
+fl.client.start_numpy_client(server_address=server_address, client=Client(None))
--- server.py
+++ server.py
@@ -1,8 +1,72 @@
-import flwr as fl
+import json
+from typing import List, Tuple
 
+import flwr as fl
+from flwr.common import Metrics, ndarrays_to_parameters, parameters_to_ndarrays
+import tensorflow as tf
+from tensorflow import keras
+
+# Hyperparameters
+# fl-specific
+server_address = '0.0.0.0:8080'
+num_rounds = 3
+min_fit_clients = 2
+min_available_clients = 2
+# model-specific
+optimizer = 'adam'
+loss = 'sparse_categorical_crossentropy'
+metrics = 'accuracy'
+batch_size = 32
+local_epochs = 2
+
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(self, rnd, results, failures):
+        aggr_weights, aggr_metrics = super().aggregate_fit(rnd, results, failures)
+        self.aggr_weights = parameters_to_ndarrays(aggr_weights)
+        return aggr_weights, aggr_metrics
+
+def fit_config(rnd: int):
+    config = {
+        "batch_size": batch_size,
+        "local_epochs": local_epochs,
+        "model": model_config,
+        "optimizer": optimizer, 
+        "loss": loss, 
+        "metrics": metrics
+    }
+    return config
+
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    accuracies = [num_examples * m['accuracy'] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+    return {'accuracy': sum(accuracies) / sum(examples)}
+
+# Model
+input = keras.layers.Input(shape=(28,28))
+x = keras.layers.Flatten(input_shape=(28,28))(input)
+x = keras.layers.Dense(128, activation='relu')(x)
+x = keras.layers.Dense(256, activation='relu')(x)
+output = keras.layers.Dense(10, activation='softmax')(x)
+
+model_arch = keras.Model(input, output)
+model = model_arch 
+
+model.compile(optimizer, loss, metrics=[metrics])
+
+model_config = json.dumps(model.get_config())
+
+# Create strategy
+strategy = SaveModelStrategy(
+    min_fit_clients=min_fit_clients,
+    min_available_clients=min_available_clients,
+    on_fit_config_fn=fit_config,
+    evaluate_metrics_aggregation_fn=weighted_average,
+    initial_parameters=ndarrays_to_parameters(model.get_weights()),
+)
 
 # Start Flower server
 fl.server.start_server(
-    server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
+    server_address=server_address,
+    config=fl.server.ServerConfig(num_rounds=num_rounds),
+    strategy=strategy
 )
